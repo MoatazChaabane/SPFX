d9b4eec518c6c49551375fc4365351e680e4127b
Moataz <chaabanemoataz@outlook.fr>
2023-12-13 16:19:04 +0100

    MAj

--- src/webparts/sol1/components/Sol1.tsx
+++ src/webparts/sol1/components/Sol1.tsx
@@ -2,7 +2,14 @@ import * as React from 'react';
 import styles from './Sol1.module.scss';
 import { escape } from '@microsoft/sp-lodash-subset';
 import { WebPartContext } from '@microsoft/sp-webpart-base';
-import { SPHttpClient } from '@microsoft/sp-http';
+import { SPHttpClient, SPHttpClientResponse } from '@microsoft/sp-http';
+
+interface IPerson {
+  Title: string;
+  Email: string;
+  Age: number;
+  ID: number;
+}
 
 export interface ISol1Props {
   description: string;
@@ -16,6 +23,7 @@ export interface ISol1Props {
 
 interface ISol1State {
   nom: string;
+  Email: string; 
   age: string;
   ageErrorMessage: string;
 }
@@ -28,8 +36,51 @@ export default class Sol1 extends React.Component<ISol1Props, ISol1State> {
       nom: '',
       age: '',
       ageErrorMessage: '',
+      Email: ''
     };
   }
+  
+
+
+  componentDidMount() {
+    this.toUpdate();
+  }
+
+  private toUpdate(): void {
+    const itemId = localStorage.getItem("ID");
+    const url = `https://mch12.sharepoint.com/sites/ABC/_api/web/lists/getbytitle('personne')/items?$filter=ID eq ${itemId}`;
+
+    // GET Api
+    this.props.context.spHttpClient.get(url, SPHttpClient.configurations.v1, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+      .then((response: SPHttpClientResponse) => {
+        console.log(`Status code: ${response.status}`);
+        console.log(`Status text: ${response.statusText}`);
+
+        if (response.ok) {
+          response.json().then((responseJSON: { value: IPerson[] }) => {
+            console.log(responseJSON);
+            // Assuming you want to update the state with the data of the first item
+            if (responseJSON.value.length > 0) {
+              const { Title, Age, Email } = responseJSON.value[0];
+              this.setState({ nom: Title, age: Age.toString(), Email: Email });
+            } else {
+              console.warn('No data found for the specified ID.');
+            }
+          });
+        } else {
+          console.error('Error:', response.statusText);
+        }
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+  
 
   private handleNameChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     this.setState({ nom: event.target.value });
@@ -47,6 +98,69 @@ export default class Sol1 extends React.Component<ISol1Props, ISol1State> {
     }
   };
 
+  private handleEmailChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    this.setState({ Email: event.target.value });
+  };
+// UPDATE DATA
+  private async updateData(): Promise<void> {
+    const { nom, age } = this.state;
+    const mail = (document.getElementById('email') as HTMLInputElement).value;
+
+    // Vérifiez l'âge avant de mettre à jour les données
+    if (age !== '' && parseInt(age, 10) < 18) {
+      this.setState({ ageErrorMessage: "You must be 18 or older to proceed." });
+      return;
+    }
+
+    // Vérifiez le nom et le courriel
+    if (nom === '' || !(/^[A-Za-z\s]+$/.test(nom))) {
+      alert('le nom est vide');
+      return;
+    } else if (mail === '' || !(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(mail))) {
+      alert("Verify email");
+      return;
+    } else if (!age) {
+      alert("Verify age");
+      return;
+    }
+
+    const itemId = localStorage.getItem("ID");
+
+    // Mettre à jour les données avec la méthode POST et X-HTTP-Method: MERGE
+    const url = `https://mch12.sharepoint.com/sites/ABC/_api/web/lists/getbytitle('personne')/items(${itemId})`;
+
+    const itemBody = {
+      'Title': nom,
+      'Email': mail,
+      'Age': age
+    };
+    console.log(JSON.stringify(itemBody));
+    
+
+    try {
+      const postResponse = await this.props.context.spHttpClient.post(url, SPHttpClient.configurations.v1, {
+        headers: {
+          'Accept': 'application/json',
+          'Content-Type': 'application/json',
+          'IF-MATCH': '*',
+          'X-HTTP-Method': 'MERGE'  // Use MERGE to update an existing item
+        },
+        body: JSON.stringify(itemBody),
+      });
+
+      if (postResponse.ok) {
+        const postResponseJSON = await postResponse.json();
+        console.log('POST Response:', postResponseJSON);
+        this.props.redirectTo();
+      } else {
+        throw new Error(`POST Error: ${postResponse.statusText}`);
+      }
+    } catch (postError) {
+      console.error('POST Error:', postError);
+    }
+  }
+
+// add data
   private async sendData(): Promise<void> {
     const { nom, age } = this.state;
     const mail = (document.getElementById('email') as HTMLInputElement).value;
@@ -64,8 +178,8 @@ export default class Sol1 extends React.Component<ISol1Props, ISol1State> {
     } else if (mail === '' || !(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(mail))) {
       alert("Verify email");
       return;
-    }else if (!age ) {
-      alert ("Verify age");
+    } else if (!age) {
+      alert("Verify age");
       return;
     }
 
@@ -113,22 +227,27 @@ export default class Sol1 extends React.Component<ISol1Props, ISol1State> {
             <tbody>
               <tr>
                 <td><b>Name</b></td>
-                <td><input className="form-control" type='text' id='name' onChange={this.handleNameChange} required /></td>
+                <td><input className="form-control" type='text' id='name' onChange={this.handleNameChange} value={this.state.nom} required /></td>
               </tr>
               <tr>
                 <td><b>Email</b></td>
-                <td><input className="form-control" type='email' id='email' required /></td>
+                <td><input className="form-control" type='email' id='email' onChange={this.handleEmailChange} value={this.state.Email} required /></td>
               </tr>
               <tr>
                 <td><b>Age</b></td>
                 <td>
-                  <input className="form-control" type='number' id='age' onChange={this.handleAgeChange} required />
+                  <input className="form-control" type='number' id='age' onChange={this.handleAgeChange} value={this.state.age} required />
                   {ageErrorMessage && <p style={{ color: 'red' }}>{ageErrorMessage}</p>}
                 </td>
               </tr>
             </tbody>
           </table>
-          <button className="btn btn-primary" onClick={() => this.sendData()}>Send</button>
+          {localStorage.getItem("ID") ? (
+            <button className="btn btn-primary" onClick={() => this.updateData()}>Update</button>
+            ) : (
+            <button className="btn btn-primary" onClick={() => this.sendData()}>Send</button>
+
+          )}
         </div>
       </section>
     );
--- src/webparts/sol1/components/list.tsx
+++ src/webparts/sol1/components/list.tsx
@@ -7,6 +7,7 @@ interface IPerson {
   Title: string;
   Email: string;
   Age: number;
+  ID: number;
 }
 
 export interface IListProps {
@@ -31,7 +32,11 @@ export default class List extends React.Component<IListProps, IListState> {
       people: [],
     };
   }
+  private async onEdit($id: number): Promise<void> {
+    localStorage.setItem("ID", String($id));
 
+    this.props.redirectTo();
+  }
   private async onReturn(): Promise<void> {
     this.props.redirectTo();
   }
@@ -41,7 +46,7 @@ export default class List extends React.Component<IListProps, IListState> {
   }
 
   private getData(): void {
-    const url = `https://mch12.sharepoint.com/sites/ABC/_api/web/lists/getbytitle('personne')/items`;
+    const url = `https://mch12.sharepoint.com/sites/ABC/_api/web/lists/getbytitle('personne')/items?$top=1000`;
 
     // GET Api
     this.props.context.spHttpClient.get(url, SPHttpClient.configurations.v1, {
@@ -81,6 +86,7 @@ export default class List extends React.Component<IListProps, IListState> {
                 <th scope="col">Title</th>
                 <th scope="col">Email</th>
                 <th scope="col">Age</th>
+                <th scope="col">Action</th>
               </tr>
             </thead>
             <tbody>
@@ -89,6 +95,13 @@ export default class List extends React.Component<IListProps, IListState> {
                   <td>{person.Title}</td>
                   <td>{person.Email}</td>
                   <td>{person.Age}</td>
+                  <td>{person.ID}</td>
+                  <td><button onClick={() => this.onEdit(person.ID)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pen" viewBox="0 0 16 16">
+  <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+</svg></button></td>
+<td><button><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
+  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+</svg></button></td>
                 </tr>
               ))}
             </tbody>
